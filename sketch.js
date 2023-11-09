7a3c314f1725ebd62cea5573ecf8db32f5a1bd02
Saiteja-Kudligi <149548893+Saiteja-Kudligi@users.noreply.github.com>
2023-11-08 23:12:00 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,86 +1,61 @@
 let cities;
-let zoomLevel = 1;
-let zoomInBtn, zoomOutBtn, zoomPercentage, zoomInput;
-let xoffset = 0;
-let yoffset = 0; // Store the y-offset for panning
-let colors = {}; // Object to store colors for countries
+let earth;
+let r = 250;
+let colors = {};
+let rotationAngle = 0;
 
 function preload() {
-  cities = loadTable("worldcities.csv", "header");
+  earth = loadImage('Earthg.jpg');
+  cities = loadTable('worldcities.csv', 'header');
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  fill(255, 150);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
 
-  // Function to generate a random color
-  function randomColor() {
-    return color(random(255), random(255), random(255));
+  let countryColor = Array.from(new Set(cities.getColumn('country')));
+  for (let i = 0; i < countryColor.length; i++) {
+    colors[countryColor[i]] = color(random(255), random(255), random(255));
   }
-
-  // Assign a random color to each unique country
-  let uniqueCountries = Array.from(new Set(cities.getColumn("country")));
-  for (let i = 0; i < uniqueCountries.length; i++) {
-    colors[uniqueCountries[i]] = randomColor();
-  }
-
-  zoomInBtn = select('#zoomInBtn');
-  zoomOutBtn = select('#zoomOutBtn');
-  zoomPercentage = select('#zoomPercentage');
-  zoomInput = select('#zoomInput');
-
-  zoomInBtn.mousePressed(zoomIn);
-  zoomOutBtn.mousePressed(zoomOut);
-  zoomInput.input(changeZoomLevel);
-  frameRate(60);
 }
 
 function draw() {
   background(0);
-  translate(xoffset, yoffset);
-  scale(10 * zoomLevel);
-  for (let i = 0; i < cities.getRowCount(); i++) {
-    let country = cities.getString(i, "country");
-    let latitude = cities.getNum(i, "lat");
-    let longitude = cities.getNum(i, "lng");
-    setXY(latitude, longitude, colors[country]); // Assign the country's color
+  rotateY(rotationAngle);
+
+  texture(earth);
+
+  sphere(r);
+
+  for (let row of cities.rows) {
+    let lat = row.getNum('lat');
+    let lon = row.getNum('lng');
+    let country = row.getString('country');
+
+    let angle = radians(lat);
+    let phi = radians(lon) + PI - radians(90);
+    let x = r * cos(angle) * cos(phi);
+    let y = -r * sin(angle);
+    let z = -r * cos(angle) * sin(phi);
+
+    let h = 0.5; 
+
+    push();
+    translate(x, y, z);
+    fill(colors[country]);
+    sphere(h);
+    pop();
   }
-
-  zoomPercentage.html("Zoom: " + (zoomLevel * 100).toFixed(0) + "%");
-}
-
-function setXY(lat, lng, col) {
-  let x = map(lng, -180, 180, 0, width);
-  let y = map(lat, 90, -90, 0, height);
-  fill(col);
-  ellipse(x, y, 0.25, 0.25);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function zoomIn() {
-  zoomLevel += 0.1;
-  zoomLevel = constrain(zoomLevel, 0.1, 10);
-  zoomInput.value(zoomLevel);
-}
-
-function zoomOut() {
-  zoomLevel -= 0.1;
-  zoomLevel = constrain(zoomLevel, 0.1, 10);
-  zoomInput.value(zoomLevel);
-}
-
-function changeZoomLevel() {
-  zoomLevel = float(zoomInput.value());
-  zoomLevel = constrain(zoomLevel, 0.1, 10);
-}
-
-function mouseDragged() {
-  if (mouseButton === LEFT) {
-    xoffset += mouseX - pmouseX;
-    yoffset += mouseY - pmouseY;
+function keyPressed() {
+  if (key === 'ArrowLeft') {
+    rotationAngle -= 0.2;
+  } else if (key === 'ArrowRight') {
+    rotationAngle += 0.2;
   }
 }
